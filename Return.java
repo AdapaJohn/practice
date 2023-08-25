5d0bb97ded9fdb121734bc03e46b7a3621a86bde
Adapa John <132468205+AdapaJohn@users.noreply.github.com>
2023-08-25 12:25:44 +0530

    Add files via upload

---
+++ Return.java
@@ -0,0 +1,12 @@
+public class Return {
+    public  int  Add(){
+
+return 34;
+    }
+    public static void main(String[] args) {
+        Return obj  =new Return();
+        System.out.println(obj.Add());
+        
+    }
+    
+}
